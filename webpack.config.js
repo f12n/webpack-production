1aed78583b69589b80e9867a65aa8e80ee4dec77
rockGithub <rock20110414@gmail.com>
2018-05-22 11:27:03 +0800

    cleaning up the dist/ folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // npx webpack --config webpack.config.js
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
@@ -12,8 +14,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output management'
+            title: 'Output Management'
         })
     ]
 };
